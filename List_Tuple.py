0d4dfefa3a79fcf6541cb41daa4eb2c42c46cef1
bavabala <62609269+bavabala@users.noreply.github.com>
2022-07-23 19:01:18 +0530

    Add files via upload
    Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples

---
+++ List_Tuple.py
@@ -0,0 +1,10 @@
+length = [(2,5),(1,2),(4,4),(2,3),(2,1)]
+# For Loop:
+for i in range(len(length)):
+    for j in range(len(length)):
+#If Condition:
+        if length[i][1] < length[j][1]:
+            length[i],length[j] = length[j],length[i]
+#print:
+print(length)
+            
